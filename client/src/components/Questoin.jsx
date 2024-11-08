c9401a83ba67d697e1d273663084034a8c36bb94
Mukhammad <jonbola1999@gmail.com>
2024-11-08 15:29:28 +0300

    release_4

--- client/src/components/Questoin.jsx
+++ client/src/components/Questoin.jsx
@@ -1,41 +1,53 @@
 import { useEffect, useState } from "react";
 import Answer from "./Answer";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Questoin() {
   const [questions, setQuestions] = useState([]);
-  const [curQ, setCurQ] = useState(0);
+  // const [curQ, setCurQ] = useState(0);
+  const navigate = useNavigate();
 
   const { themeId } = useParams();
-  console.log(themeId);
 
   useEffect(() => {
-    loadQuestions();
+    handleQuestions();
   }, []);
 
-  async function loadQuestions() {
+  async function handleQuestions() {
     const response = await fetch(`/api/questions/${themeId}`);
     const data = await response.json();
+    console.log(data);
     setQuestions(data);
   }
 
   return (
     <>
       <div className="card text-center">
-        <div className="card-header">Quizzz</div>
+        <div className="card-header"></div>
         <div className="card-body">
-          <h5 className="card-title mb-3">Вопрос НЕКИЙ</h5>
+          <h5 className="card-title mb-3">Вопросы:</h5>
 
-          <a href="#" className="btn btn-primary">
-            Следущий вопрос
-          </a>
+          <button
+            // key={topic.id}
+            className="btn btn-primary"
+            type="button"
+            onClick={() => navigate("/")}
+          >
+            Выбрать другую тему
+          </button>
         </div>
-        <div className="card-footer text-body-secondary">
-          Выберите правильный ответ
-        </div>
+
         {questions.length > 0 ? (
           questions.map((current) => (
-            <Answer key={current.id} testProps={current} />
+            <Answer
+              key={current.id}
+              question_name={current.question_name}
+              id={current.id}
+              answer1={current.answer1}
+              answer2={current.answer2}
+              answer3={current.answer3}
+              answer4={current.answer3}
+            />
           ))
         ) : (
           <div>Ответы не загружены</div>
